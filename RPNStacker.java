49450223576280ebd37b1203d0d01dab050764d6
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-11-25 22:16:13 -0300

    Task 2

--- RPNStacker.java
+++ RPNStacker.java
@@ -1,38 +1,60 @@
+package RPN;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 import java.io.FileNotFoundException;
 
 class Main {
   public static void main(String args[]) throws FileNotFoundException { 
-    Scanner scan = new Scanner(new File("Calc1.stk")); //replace file name here
+    Scanner scan = new Scanner(new File("Calc1.stk"));
     Stack<Integer> stack = new Stack<Integer>();
+    
+    List<Token> tokens = new ArrayList<Token>();
 
     while(scan.hasNextLine()){
-	        String line = scan.nextLine();
-	        try {
-	        	int i = Integer.parseInt(line);
-	        	stack.push(i);
-	        }
-	        catch (Exception e)
-	        {
-	        	int top = stack.pop();
-	        	int bot = stack.pop();
-	        	
-	        	if (line.equals("*")) {
-	        		stack.push(bot*top);
-	        	}
-	        	else if (line.equals("/")) {
-	        		stack.push(bot/top);
-	        	}
-	        	else if (line.equals("-")) {
-	        		stack.push(bot-top);
-	        	}
-	        	else if (line.equals("+")) {
-	        		stack.push(bot+top);
-	        	}
-	        }
-	    }
+    	if (scan.hasNextInt())
+    	{
+    		String num = scan.nextLine();
+    		tokens.add(new Token(TokenType.NUM, num));
+    		stack.push(Integer.parseInt(num));
+    	} else {
+    		String value = scan.nextLine();
+        	
+        	if (value.equals("*")) {
+        		int top = stack.pop();
+            	int bot = stack.pop();
+        		stack.push(bot*top);
+        		tokens.add(new Token(TokenType.STAR, value));
+        	}
+        	else if (value.equals("/")) {
+        		int top = stack.pop();
+            	int bot = stack.pop();
+        		stack.push(bot/top);
+        		tokens.add(new Token(TokenType.SLASH, value));
+        	}
+        	else if (value.equals("-")) {
+        		int top = stack.pop();
+            	int bot = stack.pop();
+        		stack.push(bot-top);
+        		tokens.add(new Token(TokenType.MINUS, value));
+        	}
+        	else if (value.equals("+")) {
+        		int top = stack.pop();
+            	int bot = stack.pop();
+        		stack.push(bot+top);
+        		tokens.add(new Token(TokenType.PLUS, value));
+        	} else {
+        		System.out.println("Error: Unexpected character: " + value);
+        		return;
+        	}
+    	}
+    }
 	    System.out.println(stack.pop());
+	    for (int i = 0; i < tokens.size(); i++)
+	    {
+	    	System.out.println(tokens.get(i).toString());
+	    }
   } 
 }
